dbf859f61b6675ebff1487980250719cac694418
Casy-wav <colin.harr21@gmail.com>
2025-06-22 10:58:11 +0200

    Homepage, Discord fertig Socials angefangen

--- PA_django/landingpage/urls.py
+++ PA_django/landingpage/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("discord/", views.discord, name="discord"),
+    path("socials/", views.socials, name="socials"),
 ]
--- PA_django/landingpage/views.py
+++ PA_django/landingpage/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def discord(request):
+    return render(request, 'discord.html')
+
+def socials(request):
+    return render(request, 'socials.html')
