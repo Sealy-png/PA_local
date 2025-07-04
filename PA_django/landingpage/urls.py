ccb1d768da8e1615e614c931a8fd376d81f674fc
Casy-wav <colin.harr21@gmail.com>
2025-07-04 13:07:01 +0200

    Alle main pages fertig, register und legal pages fehlen

--- PA_django/landingpage/urls.py
+++ PA_django/landingpage/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("discord/", views.discord, name="discord"),
     path("socials/", views.socials, name="socials"),
+    path("supported_brokers/", views.supported_brokers, name="supported_brokers"),
+    path("pricing/", views.pricing, name="pricing"),
+    path("about/", views.about, name="about"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
 ]
--- PA_django/landingpage/views.py
+++ PA_django/landingpage/views.py
@@ -4,8 +4,20 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render(request, 'home.html')
 
-def discord(request):
-    return render(request, 'discord.html')
-
 def socials(request):
     return render(request, 'socials.html')
+
+def supported_brokers(request):
+    return render(request, 'supported_brokers.html')
+
+def pricing(request):
+    return render(request, 'pricing.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
